317a2ef93842370ad8b36a6bd9ac294cf57171d7
Bartosz <elitarform@gmail.com>
2019-05-13 16:17:41 +0200

    Add services registration

---
+++ BlikPrismApp/BlikPrismApp/AppBootstrapper.cs
@@ -0,0 +1,30 @@
+﻿using BlikPrismApp.Services.Blik;
+using BlikPrismApp.Services.SignIn;
+using Splat;
+
+namespace BlikPrismApp
+{
+    public class AppBootstrapper : IEnableLogger
+    {
+        public AppBootstrapper() => RegisterDependencies();
+
+        public void RegisterDependencies()
+        {
+
+
+            #region PAGES
+            containerRegistry.RegisterForNavigation<AccountPage, AccountPageViewModel>();
+            containerRegistry.RegisterForNavigation<SignInPage, SignInPageViewModel>();
+
+            containerRegistry.RegisterForNavigation<BlikCodePage, BlikCodePageViewModel>();
+            containerRegistry.RegisterForNavigation<BlikConfirmationPage, BlikConfirmationPageViewModel>();
+            #endregion
+
+            #region SERVICES
+            Locator.CurrentMutable.Register(() => new SignInService(), typeof(ISignInService));
+            Locator.CurrentMutable.Register(() => new BlikService(), typeof(IBlikService));
+            #endregion
+        }
+    }
+}
+
